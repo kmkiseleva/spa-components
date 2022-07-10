ba5489eb938517b2162a2ac92e0a855c65bcd9a0
Ksenia Kiseleva <ksushakiseleva@mail.ru>
2022-07-10 15:31:32 +0300

    save

--- src/components/ProductCard/MallProductCard.tsx
+++ src/components/ProductCard/MallProductCard.tsx
@@ -1,10 +1,7 @@
 import React, { FC, useState } from "react";
 import "./MallProductCard.css";
 import { IMallProductCard } from "./IMallProductCard";
-import { ReactComponent as Compare } from "../../assets/icon-compare.svg";
-import { ReactComponent as Heart } from "../../assets/icon-heart.svg";
 import testImg from "../../assets/test-img.jpg";
-import { ReactComponent as Cart } from "../../assets/icon-cart.svg";
 
 const MallProductCard: FC<IMallProductCard> = ({
   title = "Диван Лени Textile Rustic",
@@ -57,11 +54,27 @@ const MallProductCard: FC<IMallProductCard> = ({
         {active && (
           <div className="mall-labels__block">
             <button onClick={onClickCompareButton}>
-              <Compare style={{ fill: textSecondaryColor }} />
+              <svg
+                width="19"
+                height="19"
+                viewBox="0 0 19 19"
+                fill={textSecondaryColor}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0.5 10H2.5V19H0.5V10ZM16.5 6H18.5V19H16.5V6ZM8.5 0H10.5V19H8.5V0Z" />
+              </svg>
             </button>
 
             <button onClick={onClickFavoritesButton}>
-              <Heart style={{ fill: textSecondaryColor }} />
+              <svg
+                width="21"
+                height="19"
+                viewBox="0 0 21 19"
+                fill={textSecondaryColor}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15 0C18.038 0 20.5 2.5 20.5 6C20.5 13 13 17 10.5 18.5C8 17 0.5 13 0.5 6C0.5 2.5 3 0 6 0C7.86 0 9.5 1 10.5 2C11.5 1 13.14 0 15 0ZM11.434 15.604C12.315 15.048 13.11 14.495 13.854 13.903C16.835 11.533 18.5 8.943 18.5 6C18.5 3.64 16.963 2 15 2C13.924 2 12.76 2.57 11.914 3.414L10.5 4.828L9.086 3.414C8.24 2.57 7.076 2 6 2C4.06 2 2.5 3.656 2.5 6C2.5 8.944 4.166 11.533 7.145 13.903C7.89 14.495 8.685 15.048 9.566 15.603C9.865 15.792 10.161 15.973 10.5 16.175C10.839 15.973 11.135 15.792 11.434 15.604Z" />
+              </svg>
             </button>
           </div>
         )}
@@ -110,7 +123,19 @@ const MallProductCard: FC<IMallProductCard> = ({
           </button>
         ) : (
           <button className="mall-button__icon" onClick={onClickCartButton}>
-            <Cart style={{ fill: accentColor }} />
+            <svg
+              width="46"
+              height="46"
+              viewBox="0 0 46 46"
+              fill={accentColor}
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0 4C0 1.79086 1.79086 0 4 0H42C44.2091 0 46 1.79086 46 4V42C46 44.2091 44.2091 46 42 46H4C1.79086 46 0 44.2091 0 42V4Z" />
+              <path
+                d="M18 19V17C18 15.6739 18.5268 14.4021 19.4645 13.4645C20.4021 12.5268 21.6739 12 23 12C24.3261 12 25.5979 12.5268 26.5355 13.4645C27.4732 14.4021 28 15.6739 28 17V19H31C31.2652 19 31.5196 19.1054 31.7071 19.2929C31.8946 19.4804 32 19.7348 32 20V32C32 32.2652 31.8946 32.5196 31.7071 32.7071C31.5196 32.8946 31.2652 33 31 33H15C14.7348 33 14.4804 32.8946 14.2929 32.7071C14.1054 32.5196 14 32.2652 14 32V20C14 19.7348 14.1054 19.4804 14.2929 19.2929C14.4804 19.1054 14.7348 19 15 19H18ZM18 21H16V31H30V21H28V23H26V21H20V23H18V21ZM20 19H26V17C26 16.2044 25.6839 15.4413 25.1213 14.8787C24.5587 14.3161 23.7956 14 23 14C22.2044 14 21.4413 14.3161 20.8787 14.8787C20.3161 15.4413 20 16.2044 20 17V19Z"
+                fill="white"
+              />
+            </svg>
           </button>
         )}
       </div>
